c12bd06ad90e47d019d31e7336bde4684e58337c
urooj khan <uroojk844@gmail.comm>
2024-07-10 18:44:10 +0530

    Added vercel file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express()
 const mongoose = require('mongoose')
+app.get("/",(req,res)=>{
+    res.json("Hemlo")
+})
 mongoose.connect("mongodb+srv://uroojk844:Erp123@cluster0.d7alzlz.mongodb.net/social?retryWrites=true&w=majority&appName=Cluster0")
 .then(connected=>console.log("Mongo DB connected"))
 .catch(err=>console.log(err))
