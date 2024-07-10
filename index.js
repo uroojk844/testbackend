e40401435a487a32d6c2f2720f9ef770ff8c31f7
urooj khan <uroojk844@gmail.comm>
2024-07-10 18:40:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express()
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://uroojk844:Erp123@cluster0.d7alzlz.mongodb.net/social?retryWrites=true&w=majority&appName=Cluster0")
+.then(connected=>console.log("Mongo DB connected"))
+.catch(err=>console.log(err))
+app.listen(5050,()=>console.log("App is running"));
